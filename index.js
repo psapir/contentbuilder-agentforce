f9966674a76ee5af526ae35f3a0864124bb3c8de
Pato Sapir <patriciosapir@gmail.com>
2025-11-05 10:01:42 -0300

    removing vercel

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ app.get('/', function(request, response) {
 
 app.use('/',express.static('dist'));
 
-if (require.main === module) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-}
